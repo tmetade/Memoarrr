dac20a73ed5ae229389be27f12c808a6f6e5985f
tmetade <tmetade@yahoo.ca>
2018-10-17 10:40:36 -0400

    initial project commit and initial user parameters

---
+++ CSI2372Project/CSI2372Project/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    int gameVersion = 0, nPlayers = 0;
+    
+    while(gameVersion<1 || gameVersion > 3)
+    {
+        std::cout << "Select your game version: " <<std::endl;
+        std::cout << "1-Base Game 2-Expert Display Mode 3-Expert Rules Mode ";
+        std::cin >> gameVersion;
+    }
+    
+    while(nPlayers<2 || nPlayers>4)
+    {
+        std::cout << "Enter the number of players (2-4): ";
+        std::cin >> nPlayers;
+    }
+    
+    std::string playerNames[nPlayers];
+    std::string tempPlayerName;
+    std::cout << "Enter the name of the players: ";
+    for(int i=0; i<nPlayers; i++)
+    {
+        std::cin >> tempPlayerName;
+        playerNames[i] = tempPlayerName;
+    }
+    return 0;
+}
